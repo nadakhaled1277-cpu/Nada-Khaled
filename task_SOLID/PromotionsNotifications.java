1218e60aa2d4d2c115ff6cbf30b2f71a4ee785e9
nadakhaled1277-cpu <nadakhaled1277@gmail.com>
2025-12-06 04:17:37 +0200

    .

--- task_SOLID/PromotionsNotifications.java
+++ task_SOLID/PromotionsNotifications.java
@@ -1,5 +1,5 @@
 import java.time.LocalDate;
-public class PromotionsNotifications {
+public class PromotionsNotifications implements{
     private String id;
     private String userId;
     private String target;
