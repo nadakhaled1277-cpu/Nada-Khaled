f0c57cc67862044afc69e848eb131e86d3daee1f
nadakhaled1277-cpu <nadakhaled1277@gmail.com>
2025-12-06 04:20:51 +0200

    .

--- task_SOLID/Notification.java
+++ task_SOLID/Notification.java
@@ -4,6 +4,7 @@ public interface Notification {
     public String getId(); 
     public String getUserId(); 
     public String getTarget(); 
-    public String getType(); 
+    public String getType();
+    public String getPreferredChannel();
     public void validate();
 }
